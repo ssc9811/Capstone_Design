0ca432204d6930b095c171014e83be66291a57aa
ssc9811 <poer2418@naver.com>
2020-09-25 18:19:16 +0900

    별자리 운세 추가 [수정중]

--- project/main.py
+++ project/main.py
@@ -1,8 +1,10 @@
 from flask import Flask , render_template, request, redirect
 from google_news import search_news, headline_news
 from naver_weather import naver_weather
+from fortune import extract_fortune
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
 def home():
@@ -11,7 +13,7 @@ def home():
     return render_template("home.html", headline=headline, weather=weather)
 
 @app.route("/report")
-def name():
+def news():
     # print(request.args.get('name'))
     word = request.args.get('name')
     if word:
@@ -21,8 +23,14 @@ def name():
         return redirect("/")
     return render_template("report.html", news_title=word, titles=titles)
 
-# @app.route("/<name>")
-# def username(name):
-#     return f"당신의 이름은 {name}"
+@app.route("/movies")
+def movies():
+    return render_template("movies.html")
+
+@app.route("/fortune")
+def fortune():
+    value = request.args.get('selectBox')
+    result = extract_fortune(value)
+    return render_template("fortune.html", word = value, result = result)
 
 app.run(host="127.0.0.1")
--- project/test.py
+++ project/test.py
@@ -1,9 +1,12 @@
-from naver_weather import naver_weather
-from google_news import headline_news
+# from naver_weather import naver_weather
+# from google_news import headline_news
 
-# weather = naver_weather()
-# print(weather.keys())
-# print(weather.values())
+# # weather = naver_weather()
+# # print(weather.keys())
+# # print(weather.values())
 
-s = headline_news()
-print(type(dict(s)))
+# s = headline_news()
+# print(type(dict(s)))
+
+
+
