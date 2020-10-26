a140189d680c114bcd8689b946b73b37f8157353
ssc9811 <poer2418@naver.com>
2020-10-27 00:20:16 +0900

    기사검색 버그 수정, 배치 변경

--- project/main.py
+++ project/main.py
@@ -1,45 +1,40 @@
 from flask import Flask , render_template, request, redirect
 from google_news import search_news, headline_news
 from naver_weather import naver_weather
-# from fortune import extract_fortune
-# from google_movies import google_movies
+from fortune import extract_fortune
+from google_movies import google_movies
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/")
+@app.route('/')
 def home():
     headline = headline_news()
     weather = naver_weather()
-    word = request.args.get('name')
+    return render_template("home.html", headline=headline, weather=weather)
+
+@app.route("/report")
+def news():
+    headline = headline_news()
+    weather = naver_weather()
+    word = request.args.get('title')
     if word:
         word = word.lower()
         titles = search_news(word)
     else:
         return redirect("/")
-    return render_template("home.html", headline=headline, weather=weather, news_title=word, titles=titles)
+    return render_template("home.html", news_title=word, titles=titles, headline=headline, weather=weather)
 
-# @app.route("/")
-# def news():
-#     word = request.args.get('name')
-#     if word:
-#         word = word.lower()
-#         titles = search_news(word)
-#         print(type(word))
-#     else:
-#         return redirect("/")
-#     return render_template("home.html", news_title=word, titles=titles)
+@app.route("/movies")
+def movies():
+    movies = google_movies()
+    return render_template("movies.html", movies=movies)
 
-# @app.route("/movies")
-# def movies():
-#     movies = google_movies()
-#     return render_template("movies.html", movies=movies)
-
-# @app.route("/fortune")
-# def fortune():
-#     value = request.args.get('selectBox')
-#     # print(type(value))
-#     result = extract_fortune(value)
-#     return render_template("test.html", value=value, result=result)
+@app.route("/fortune")
+def fortune():
+    value = request.args.get('selectBox')
+    # print(type(value))
+    result = extract_fortune(value)
+    return render_template("fortune.html", result=result)
 
 app.run(host="127.0.0.1")
